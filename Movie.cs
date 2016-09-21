f3d8195bb7c862a27f2a0a8503ddc5eb96390110
a-let <aletinski@yahoo.com>
2016-09-21 16:32:16 -0400

    Add files via upload

---
+++ Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_ten
+{
+    public class Movie
+    {
+        private string _title;
+        private string _category;
+
+        public Movie(string title, string category)
+        {
+            _title = title;
+            _category = category;
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }
+    }
+}
